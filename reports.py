22a23aef44cc10f6bd5547a4da9c39c4aafe94d2
Fabian Kargl <mt221024@fhstp.ac.at>
2025-09-25 15:50:52 +0200

    fix error in Comprehensions and Assignment Expressions commit

--- reports.py
+++ reports.py
@@ -1,6 +1,7 @@
 from ledger import Ledger
 from datetime import datetime, date
 
+
 def print_summary(ledger: Ledger):
     print("=== Budget Summary ===")
     print(f"Balance: {ledger.get_balance()} €")
@@ -9,17 +10,24 @@ def print_summary(ledger: Ledger):
         print(" -", t)
 
 def get_monthly_report(ledger: Ledger, year: int, month: int):
-    print(f"=== Monatlicher Report von {month}-{year} ===")
+    print(f"=== Monatlicher Report von {month:02d}-{year} ===")
     print("Transaktionen:")
+    
+    date_format = '%Y-%m-%d'
+
     monthly_transactions = [
         t for t in ledger.transactions
-        if (t_date := (t.date if isinstance(t.date, date) else datetime.strptime(t.date, '%Y-%m-%d'))).year == year
+        if (t_date := datetime.strptime(t.date, date_format) if isinstance(t.date, str) else t.date)
+        and t_date.year == year 
         and t_date.month == month
     ]
-
+    
+    for t in monthly_transactions:
+        print(t)
+        
     monthly_expenses = sum(t.amount for t in monthly_transactions if t.type == "expense")
     monthly_income = sum(t.amount for t in monthly_transactions if t.type == "income")
     
-    print(f"Einahmen: {monthly_income}")
-    print(f"Ausgaben: {monthly_expenses}")
-    print(f"Balance: {monthly_income-monthly_expenses}")
+    print(f"\nEinahmen: {monthly_income:,.2f}")
+    print(f"Ausgaben: {monthly_expenses:,.2f}")
+    print(f"Balance: {monthly_income - monthly_expenses:,.2f}")
