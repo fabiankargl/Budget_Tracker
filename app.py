14e2f8b36a21dfe8cbbc60c75ce43b62c13039fa
Fabian Kargl <mt221024@fhstp.ac.at>
2025-09-25 15:27:37 +0200

    Classes That Should Just Be Functions or Modules

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from ledger import Ledger
 from models import Transaction, Category
-from reports import ReportGenerator
+from reports import get_monthly_report, print_summary
 from categorymanager import CategoryManager
 from utils import select_category
 import readchar
@@ -8,7 +8,6 @@ import readchar
 class BudgetApp:
     def __init__(self):
         self.ledger = Ledger()
-        self.reporter = ReportGenerator(self.ledger)
         self.cat_manager = CategoryManager()
 
     def run(self):
@@ -31,7 +30,7 @@ class BudgetApp:
             elif choice == "2":
                 self.show_transactions()
             elif choice == "3":
-                self.reporter.print_summary()
+                print_summary(ledger=self.ledger)
             elif choice == "4": 
                 category_name = input("Neue Kategorie: ")
                 category_limit = input("Kategorie Limit (Optional): ")
@@ -43,7 +42,7 @@ class BudgetApp:
             elif choice == "6":
                 year = int(input("Jahr: "))
                 month = int(input("Monat: "))
-                self.reporter.get_monthly_report(year, month)
+                get_monthly_report(ledger=self.ledger, year=year, month=month)
             elif choice == "7":
                 print("Auf Wiedersehen!")
                 self.ledger.save_to_json()
